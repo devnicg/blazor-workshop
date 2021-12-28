8e2cc224f149285564f002e49cfe2a6640fd1a9a
Nicola Goderis <goderisnicola@gmail.com>
2021-12-28 14:30:46 +0100

    Chapter 4 Completion: State Management

---
+++ save-points/00-get-started/BlazingPizza.Client/OrderState.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace BlazingPizza.Client
+{
+    public class OrderState
+    {
+        public bool ShowConfigureDialog { get; set; }
+        public Pizza ConfiguringPizza { get; set; }
+        public Order Order { get; set; } = new Order();
+        
+        
+        public void ShowConfigurePizzaDialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new Pizza()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+            ShowConfigureDialog = true;
+        }
+    
+        public void CancelConfigurePizzaDialog()
+        {
+            ConfiguringPizza = null;
+            ShowConfigureDialog = false;
+        }
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            Order.Pizzas.Add(ConfiguringPizza);
+
+            ConfiguringPizza = null;
+            ShowConfigureDialog = false;
+        }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
+
+        public void RemoveConfiguredPizza(Pizza configuredPizza)
+        {
+            Order.Pizzas.Remove(configuredPizza);
+        }
+    }
+}
